0c0717ed4b51737eebc3ae6a6158c4b1efd4e327
yohanesotka <yohanesoktayt72@gmail.com>
2025-12-08 16:19:16 +0700

    feat : controllers

--- lib/loaders.php
+++ lib/loaders.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__."/helper.php";
-require_once __DIR__."/../App/base.php";
+require_once __DIR__."/../controllers/base.php";
 require_once __DIR__."/middleware.php";
 require_once __DIR__."/../router.php";
